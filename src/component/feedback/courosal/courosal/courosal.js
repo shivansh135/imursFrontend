f4762b476a51fbc9de2100918c91e35ae8ddfd06
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-10-14 04:14:59 +0530

    first commit

--- src/component/feedback/courosal/courosal/courosal.js
+++ src/component/feedback/courosal/courosal/courosal.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function Courosal() {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './courosal.css';
+
+// import required modules
+import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
+import { Feedback } from '../feedback-cards/card';
+
+export default function App() {
   return (
-    <div>C</div>
-  )
+    <>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <Feedback/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Feedback/>
+        </SwiperSlide><SwiperSlide>
+        <Feedback/>
+        </SwiperSlide><SwiperSlide>
+        <Feedback/>
+        </SwiperSlide><SwiperSlide>
+        <Feedback/>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
--- src/component/pricing/home.js
+++ src/component/pricing/home.js
@@ -11,6 +11,10 @@ import { DecorCard } from "../product/cards/decorCard/decorCard";
 import { ProcessContainer } from "../product/cards/processCard/processCard";
 import ProductGrid from "../product/product-grid/product-grid";
 import { MainHeading } from "../headings/heading";
+import DynamicSlides from "../feedback/courosal/courosal/courosal";
+import AliceCarousel from "react-alice-carousel";
+import Carousel from "../feedback/courosal/courosal/courosal";
+
 
 
 export const Home = () => {
@@ -34,8 +38,8 @@ export const Home = () => {
 
         <MainHeading name="Nostalgia Champions"/>
 
-       
-          
+     
+        <Carousel/>
         <PartnerInvite/>
         
         
